4b37741ebdcd6dcb42998c551973c12c110b3d9e
dgjeg <3076393364@qq.com>
2025-04-09 11:11:17 +0800

    Update SymPy_plot.py

--- src/SymPy_plot.py
+++ src/SymPy_plot.py
@@ -25,12 +25,12 @@ def problem1():
         None (直接显示绘图窗口)
     """
     # TODO: 定义符号变量 x
-    
-    
+    x = sp.symbols('x')
     # TODO: 定义要绘制的表达式
-    #expr =
-    
+    expr = sp.cos(sp.tan(sp.pi * x))
     # TODO: 完成绘图函数调用
+    plot(expr, (x, -1, 1), xlabel='x', ylabel='cos(tan(pi*x))',
+         title='Problem 1: Plot of cos(tan(pi*x))')
     # 提示: 使用 plot() 函数，设置 x 的范围为 -1 到 1
     # 添加 xlabel, ylabel 和 title 参数
     pass
@@ -49,16 +49,20 @@ def problem2():
         None (直接显示绘图窗口)
     """
     # TODO: 定义符号变量x,y
-    
+    x, y = sp.symbols('x y')
     
     # TODO: 定义隐函数表达式
-    
+    expr = sp.exp(y) + sp.cos(x)/x + y
     
     # TODO: 完成隐函数绘图
     # 提示: 使用 plot_implicit() 函数
     # 设置 x 和 y 的合理范围(如 x: -10到10，y: -10到10)
     # 添加 xlabel, ylabel, title 参数
     # 设置 points=500 以获得更好的图像质量
+    plot_implicit(expr, (x, -10, 10), (y, -10, 10),
+                  xlabel='x', ylabel='y',
+                  title='Problem 2: Implicit plot of exp(y)+cos(x)/x+y=0',
+                  points=500)
     pass
 
 def problem3():
@@ -80,15 +84,21 @@ def problem3():
         None (直接显示绘图窗口)
     """
     # TODO: 定义符号变量 s,t
-    
+    s, t = sp.symbols('s t')
     
     # TODO: 定义参数方程
-    
+    x = sp.exp(-s)*sp.cos(t)
+    y = sp.exp(-s)*sp.sin(t)
+    z = t
     
     # TODO: 完成三维参数曲面绘图
     # 提示: 使用 plot3d_parametric_surface() 函数
     # 设置 s 的范围为 0 到 8，t 的范围为 0 到 5*pi
     # 添加 xlabel, ylabel, zlabel 和 title 参数
+    plot3d_parametric_surface(x, y, z, (s, 0, 8), (t, 0, 5*sp.pi),
+                              xlabel='x', ylabel='y', zlabel='z',
+                              title='Problem 3: Parametric Surface Plot')
+
     pass
 
 if __name__ == "__main__":
